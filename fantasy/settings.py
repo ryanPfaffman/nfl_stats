073c910c3b6b80d9648eec5cd95ab1b156836d88
ryanPfaffman <73614186+ryanPfaffman@users.noreply.github.com>
2020-12-12 17:39:59 -0500

    m

--- fantasy/settings.py
+++ fantasy/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/home/ryan/nfl_stats/static/'
 
 STATIC_ROOT = ('/home/ryan/nfl_stats/static/')
 
