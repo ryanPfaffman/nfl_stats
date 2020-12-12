ae9b0a4bba3a510814453e108cf2e06643918893
ryanPfaffman <73614186+ryanPfaffman@users.noreply.github.com>
2020-12-12 17:45:22 -0500

    m

--- fantasy/settings.py
+++ fantasy/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 
 STATIC_URL = ('/static/')
 
-STATIC_ROOT = ('/static/')
+STATIC_ROOT = ('/home/ryan/nfl_stats/static/')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static_in_env'),
